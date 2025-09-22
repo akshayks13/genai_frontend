2714eaf55d6ec9232a7735da007dad4be81ae52b
akshayks13 <akshayks1005@gmail.com>
2025-09-22 22:27:36 +0530

    Added new layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,19 +6,21 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import { AuthProvider } from "./providers";
 
-
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-  const hideLayout = pathname.includes("/auth");
+  const hideLayout =
+    pathname.endsWith("/auth/login") ||
+    pathname.endsWith("/auth/signup") ||
+    pathname.endsWith("/auth/forgotpassword");
 
   return (
     <html lang="en" className="scroll-smooth">
       <body className="min-h-screen bg-background font-sans antialiased">
         <AuthProvider>
           <div className="relative flex min-h-screen flex-col">
-            <Header />
+            {!hideLayout && <Header />}
             <main className="flex-1">{children}</main>
-            <Footer />
+            {!hideLayout && <Footer />}
           </div>
         </AuthProvider>
       </body>
